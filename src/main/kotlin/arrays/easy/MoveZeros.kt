d06226a825bdd45ce135fa014b9130e661694d9a
ankit.verma <ankitverma209171@gmail.com>
2024-10-23 10:40:14 +0530

    Day 2 - 23 Oct 2024

---
+++ src/main/kotlin/arrays/easy/MoveZeros.kt
@@ -0,0 +1,25 @@
+package arrays.easy
+
+/**
+ * Move Zeros to Right while maintaining the order of the non-negative numbers
+ *
+ * Link: https://takeuforward.org/data-structure/move-all-zeros-to-the-end-of-the-array/
+ */
+
+private fun main() {
+    val nums = intArrayOf(1, 0, 2, 3, 0, 4, 0, 1)
+    moveZerosToEnd(nums = nums)
+    println(nums.contentToString())
+}
+
+private fun moveZerosToEnd(nums: IntArray) {
+    var i = 0
+    var j = 0
+    while (j < nums.size) {
+        if (nums[j] == 0) ++j // skips 0
+        else nums[i++] = nums[j++]
+    }
+    while (i < nums.size) {
+        nums[i++] = 0
+    }
+}
---
+++ src/main/kotlin/arrays/easy/RotateKElements.kt
@@ -0,0 +1,53 @@
+package arrays.easy
+
+/**
+ * Rotate the array by K elements
+ *
+ * Link: https://takeuforward.org/data-structure/rotate-array-by-k-elements/
+ *
+ * Algorithm: Rotation using "Reversal Algorithm"
+ */
+
+private fun main() {
+    val nums = intArrayOf(-1, -100, 3, 99)
+    rotateKElementsLeft(nums = nums, k = 3)
+    println(nums.contentToString())
+}
+
+private fun rotateKElementsLeft(nums: IntArray, k: Int) {
+    if (k == 0) return
+    if (nums.size == 1) return
+    val n = nums.size
+    val boundK = k % n
+    // Reverse first k elements
+    reverseElements(nums = nums, low = 0, high = boundK - 1)
+    // Reverse last k elements
+    reverseElements(nums = nums, low = boundK, high = n - 1)
+    // Reverse whole array
+    reverseElements(nums = nums, low = 0, high = n - 1)
+}
+
+private fun rotateKElementsRight(nums: IntArray, k: Int) {
+    if (k == 0) return
+    if (nums.size == 1) return
+    val n = nums.size
+    val boundK = k % n
+    // Reverse first n - k elements
+    reverseElements(nums = nums, low = 0, high = n - boundK - 1)
+    // Reverse last n - k elements
+    reverseElements(nums = nums, low = n - boundK, high = n - 1)
+    // Reverse whole array
+    reverseElements(nums = nums, low = 0, high = n - 1)
+}
+
+private fun reverseElements(nums: IntArray, low: Int = 0, high: Int = nums.lastIndex) {
+    var i = low
+    var j = high
+    while (i < j) {
+        val tmp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = tmp
+        ++i
+        --j
+    }
+}
