5c20bbb167f18c27e657c4e5d7b5e4e9a4cf8bb0
ankit.verma <ankitverma209171@gmail.com>
2024-11-04 21:14:42 +0530

    Day 14 - 4 Nov 2024

---
+++ src/main/kotlin/arrays/hard/PascalTriangle.kt
@@ -0,0 +1,31 @@
+package arrays.hard
+
+/**
+ * Program to generate Pascal's Triangle
+ *
+ * Link: https://takeuforward.org/data-structure/program-to-generate-pascals-triangle/
+ */
+
+private fun main() {
+    val n = 5
+    val pascalTriangle = generatePascalTriangle(n = n)
+    println(pascalTriangle)
+}
+
+private fun generatePascalTriangle(n: Int): List<List<Int>> {
+    if (n <= 0) return emptyList()
+    val pascalTriangle = mutableListOf<List<Int>>()
+    pascalTriangle += listOf(1)
+    for (i in 1..< n) {
+        val row = mutableListOf<Int>()
+        for (j in 0..i) {
+            if (j == 0 || j == i) {
+                row += 1
+                continue
+            }
+            row += pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j]
+        }
+        pascalTriangle += row
+    }
+    return pascalTriangle
+}
