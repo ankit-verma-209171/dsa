27948cb53ca834cf5333b2bd7735cb0ca1c6f5b4
ankit.verma <ankitverma209171@gmail.com>
2024-11-24 21:14:02 +0530

    Day 34 - 24 Nov 2024

---
+++ src/main/kotlin/binarysearch/answers/KokoEatingBananas.kt
@@ -0,0 +1,43 @@
+package binarysearch.answers
+
+import kotlin.math.ceil
+
+/**
+ * Koko Eating Bananas
+ *
+ * Link: https://takeuforward.org/binary-search/koko-eating-bananas/
+ */
+
+private fun main() {
+    val bananas = intArrayOf(805306368, 805306368, 805306368)
+    val maxHours = 1000000000
+    val minimumBananasToEatPerHour = findMinimumBananasToEatPerHour(bananas, maxHours)
+    println("Minimum Bananas To Eat Per Hour: $minimumBananasToEatPerHour")
+}
+
+private fun findMinimumBananasToEatPerHour(pilesOfBananas: IntArray, maxAvailableHours: Int): Int {
+    val minBananasPerPile = 1
+    val maxBananasPerPile = pilesOfBananas.max()
+    var low = minBananasPerPile
+    var high = maxBananasPerPile
+    while (low <= high) {
+        val mid = low + (high - low) / 2
+        val timeTakenToEatAllBananas = findTimeTakenToEatAllBananas(
+            bananasToEatPerHour = mid,
+            pilesOfBananas = pilesOfBananas
+        )
+        if (timeTakenToEatAllBananas <= maxAvailableHours) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return low
+}
+
+private fun findTimeTakenToEatAllBananas(
+    bananasToEatPerHour: Int,
+    pilesOfBananas: IntArray
+): Long = pilesOfBananas.fold(0) { acc, banana ->
+    acc + ceil(banana / bananasToEatPerHour.toDouble()).toLong()
+}
