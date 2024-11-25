f11f5430d06a3b9b4df1e0b4b9441d735a5fd224
ankit.verma <ankitverma209171@gmail.com>
2024-11-25 22:29:46 +0530

    Day 35 - 25 Nov 2024

---
+++ src/main/kotlin/binarysearch/answers/MinimumDaysToMakeMBouquets.kt
@@ -0,0 +1,61 @@
+package binarysearch.answers
+
+/**
+ * Minimum days to make M bouquets
+ *
+ * Link: https://takeuforward.org/arrays/minimum-days-to-make-m-bouquets/
+ */
+
+private fun main() {
+    val blooms = intArrayOf(5, 37, 55, 92, 22, 52, 31, 62, 99, 64, 92, 53, 34, 84, 93, 50, 28)
+    val mBouquets = 8
+    val kRoses = 2
+    val minimumDaysToMakeMBouquets = findMinimumDaysToMakeMBouquets(
+        blooms = blooms,
+        mBouquets = mBouquets,
+        kRoses = kRoses
+    )
+    println(minimumDaysToMakeMBouquets)
+}
+
+private fun findMinimumDaysToMakeMBouquets(blooms: IntArray, mBouquets: Int, kRoses: Int): Int {
+    if (mBouquets.toLong() * kRoses > blooms.size) return -1
+    var low = blooms.min()
+    var high = blooms.max()
+    while (low <= high) {
+        val mid = low + (high - low) / 2
+        if (
+            canMakeMBouquetsWithGivenDays(
+                days = mid,
+                blooms = blooms,
+                mBouquets = mBouquets,
+                kRoses = kRoses
+            )
+        ) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return low
+}
+
+private fun canMakeMBouquetsWithGivenDays(
+    days: Int,
+    blooms: IntArray,
+    mBouquets: Int,
+    kRoses: Int
+): Boolean {
+    var bouquets = 0
+    var current = 0
+    blooms.forEach { bloom ->
+        if (bloom <= days) {
+            ++current
+        } else {
+            bouquets += (current / kRoses)
+            current = 0
+        }
+    }
+    bouquets += (current / kRoses)
+    return bouquets >= mBouquets
+}
